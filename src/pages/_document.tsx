af4214bb562f3653f178503ca73f8a37e067e76a
Andrew-Colman <56676505+Andrew-Colman@users.noreply.github.com>
2022-09-22 18:26:56 -0300

    Add theme.initialColorMode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
 import { ColorModeScript } from '@chakra-ui/color-mode';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+import theme from '@styles/theme';
+
 class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head />
                 <body>
-                    <ColorModeScript />
+                    <ColorModeScript
+                        initialColorMode={theme.initialColorMode}
+                    />
                     <Main />
                     <NextScript />
                 </body>
